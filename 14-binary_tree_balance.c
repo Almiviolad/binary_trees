e11685e44b01d643457219e4dad37b033dce0271
Michael Aloh <mykelaloh@gmail.com>
2023-03-29 16:48:13 -0700

    add binary height code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,3 +14,20 @@ int binary_tree_balance(const binary_tree_t *tree)
 	r = binary_tree_height(tree->right);
 	return (l - r);
 }
+/**
+*binary_tree_height - measures the height of a binary tree
+*@tree: starting node to measure from
+*Return: height of tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
