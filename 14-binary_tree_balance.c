9fbee806003fa48fdb86ab17b7ea229053e500dd
Michael Aloh <mykelaloh@gmail.com>
2023-03-29 16:37:52 -0700

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
